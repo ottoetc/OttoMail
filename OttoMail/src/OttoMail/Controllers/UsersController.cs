741ee865c2a87d8f941fc55b4eff75df2598418d
Nathan Otto <ottoetc@gmail.com>
2016-04-25 22:45:07 -0700

    Add _Layout views.

--- OttoMail/src/OttoMail/Controllers/UsersController.cs
+++ OttoMail/src/OttoMail/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using OttoMail.Models;
+using Microsoft.Data.Entity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,5 +24,48 @@ namespace OttoMail.Controllers
             var thisUser = db.Users.FirstOrDefault(users => users.UserId == id);
             return View(thisUser);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisUser = db.Users.FirstOrDefault(users => users.UserId == id);
+            return View(thisUser);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User user)
+        {
+            db.Entry(user).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisUser = db.Users.FirstOrDefault(users => users.UserId == id);
+            return View(thisUser);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisUser = db.Users.FirstOrDefault(users => users.UserId == id);
+            db.Users.Remove(thisUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
