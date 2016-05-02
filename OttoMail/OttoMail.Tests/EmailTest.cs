f358bff9409a911772dfbb092dcfeffb105afeb3
Nathan Otto <ottoetc@gmail.com>
2016-05-01 18:00:29 -0700

    Add Testing project and first test of the Email model.

---
+++ OttoMail/OttoMail.Tests/EmailTest.cs
@@ -0,0 +1,25 @@
+﻿using OttoMail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OttoMail.Tests
+{
+    public class EmailTest
+    {
+        [Fact]
+        public void GetSubjectTest()
+        {
+            //Arrange
+            var email = new Email();
+            email.Subject = "Test";
+            //Act
+            var result = email.Subject;
+
+            //Assert
+            Assert.Equal("Test", result);   
+        }
+    }
+}
