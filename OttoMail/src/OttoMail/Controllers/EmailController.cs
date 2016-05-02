77fb9e91e6ae248732748b02a5fb51af583ae5ce
Nathan Otto <ottoetc@gmail.com>
2016-05-01 17:25:10 -0700

    Connect email with user, add basic ability to compose email.

---
+++ OttoMail/src/OttoMail/Controllers/EmailController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+using OttoMail.Models;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OttoMail.Controllers
+{
+    [Authorize]
+    public class EmailController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public EmailController(
+            UserManager<ApplicationUser> userManager,
+            ApplicationDbContext db
+        )
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            return View(_db.Emails.Where(x => x.User.Id == currentUser.Id));
+        }
+        // GET: Compose
+        public IActionResult Compose()
+        {
+            return View();
+        }
+        // POST: Compose
+        [HttpPost]
+        public async Task<IActionResult> Compose(Email email)
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            email.User = currentUser;
+            _db.Emails.Add(email);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
